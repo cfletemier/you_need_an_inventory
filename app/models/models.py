e3a928d5022cca3280035176e317e9fdd9861cf2
cfletemier <cwfletem@gmail.com>
2018-02-23 18:42:52 -0500

    Fix migration shenanigans as well as add a the first migration with a new column

--- app/models/models.py
+++ app/models/models.py
@@ -14,6 +14,7 @@ class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True, name='Item_ID')
     item_type_id = db.Column(db.Integer, db.ForeignKey(ItemType.id), nullable=False, name='Type_ID')
     item_type = db.relationship('ItemType')
+    title = db.Column(db.Text, nullable=False, name='Title')
     in_possession = db.Column(db.Boolean, default=True, name='In_Possession')
     date_added = db.Column(db.Date, default=datetime.today().date(), name='Date_Added')
     date_removed = db.Column(db.Date, default=None, nullable=True, name='Date_Removed')
